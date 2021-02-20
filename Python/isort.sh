5e49eefb2dea965cb777ae64f226c7555cb55d3f
Thomas Defise <t.defise@gmail.com>
2021-02-20 12:58:27 +0100

    Added isort.sh

---
+++ Python/isort.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+isort --diff --profile black .. > result.txt
+cat result.txt
+
+numberLine=$(wc -l < result.txt)
+if [ "${numberLine}" -gt 1 ];then
+  echo "Imports are not sorted alphabetically"
+  exit 1
+fi
+
+echo "Imports are sorted alphabetically"
+exit 0
